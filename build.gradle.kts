75f6d84d2cf4a72055d767e3cc5ab5bab0c6fd96
Shivam Rajput <shivam.rajput@thoughtworks.com>
2024-01-15 17:47:36 +0530

    Shivi | updated version of spring framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,97 +1,97 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.21"
-	kotlin("plugin.spring") version "1.9.21"
-	id("jacoco")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+    id("jacoco")
 }
 
 group = "com.shiviraj.iot"
 version = "0.0.1"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(files("./libs/logging-starter-0.0.1.jar"))
-	implementation(files("./libs/mqtt-starter-0.0.1.jar"))
-	implementation("org.springframework.integration:spring-integration-mqtt:6.0.0")
-	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(files("./libs/logging-starter-0.0.1.jar"))
+    implementation(files("./libs/mqtt-starter-0.0.1.jar"))
+    implementation("org.springframework.integration:spring-integration-mqtt:6.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.13.8")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 
 // Jacoco configuration`
 jacoco {
-	toolVersion = "0.8.7"
+    toolVersion = "0.8.7"
 }
 
 tasks.test {
-	finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-	finalizedBy(tasks.jacocoTestCoverageVerification)
+    dependsOn(tasks.test)
+    finalizedBy(tasks.jacocoTestCoverageVerification)
 
 }
 
 tasks.jacocoTestCoverageVerification {
-	dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestCoverageVerification {
-	violationRules {
-		rule {
-			limit {
-				counter = "INSTRUCTION"
-				minimum = BigDecimal(0.63)
-			}
-			limit {
-				counter = "BRANCH"
-				minimum = BigDecimal(0)
-			}
-			limit {
-				counter = "LINE"
-				minimum = BigDecimal(0.68)
-			}
-			limit {
-				counter = "METHOD"
-				minimum = BigDecimal(0.32)
-			}
-			limit {
-				counter = "CLASS"
-				minimum = BigDecimal(0.50)
-			}
-		}
-	}
+    violationRules {
+        rule {
+            limit {
+                counter = "INSTRUCTION"
+                minimum = BigDecimal(0.63)
+            }
+            limit {
+                counter = "BRANCH"
+                minimum = BigDecimal(0)
+            }
+            limit {
+                counter = "LINE"
+                minimum = BigDecimal(0.68)
+            }
+            limit {
+                counter = "METHOD"
+                minimum = BigDecimal(0.32)
+            }
+            limit {
+                counter = "CLASS"
+                minimum = BigDecimal(0.50)
+            }
+        }
+    }
 }
